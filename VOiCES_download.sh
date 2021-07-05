cad0b6b34d49e7c2009ba68ee69ed05ecf22c783
Rob <robmsmt@gmail.com>
2021-07-05 18:26:37 -0400

    Create VOiCES_download.sh

---
+++ VOiCES_download.sh
@@ -0,0 +1,6 @@
+# assume you have aws cli installed
+aws s3 cp s3://lab41openaudiocorpus/VOiCES_competition.tar.gz .
+aws s3 cp s3://lab41openaudiocorpus/VOiCES_devkit.tar.gz .
+aws s3 cp s3://lab41openaudiocorpus/VOiCES_release.tar.gz .
+aws s3 cp s3://lab41openaudiocorpus/recording_data.tar.gz .
+aws s3 cp s3://lab41openaudiocorpus/test.txt .
